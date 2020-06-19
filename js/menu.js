91bc4f8f12e0a08669f16af4f0ede26268fa63c9
Camilo029 <67129929+Camilo029@users.noreply.github.com>
2020-06-18 19:40:35 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+const menu1 = document.querySelector('.menu1');
+const menu = document.querySelector('.menu-navegacion');
+
+console.log(menu)
+console.log(menu1)
+
+
+menu1.addEventListener('click', ()=>{
+	menu.classList.toggle("spread")}
